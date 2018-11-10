4431b8565e6a7ec32d1cc856bc87dfcfd0094197
Alvaro.Barrado <Alvaro.Barrado@live.u-tad.com>
2018-11-10 23:28:17 +0100

    Professor test

--- app/src/main/java/com/dsman/uaapp/Professor_Data.java
+++ app/src/main/java/com/dsman/uaapp/Professor_Data.java
@@ -29,14 +29,14 @@ public class Professor_Data extends AppCompatActivity {
         mRecyclerViewSubjects.setLayoutManager(mLayoutManagerSubjects);
 
         // specify an adapter (see also next example)
-        CardView_Professor_Data asignatura1 = new CardView_Professor_Data(ResourcesCompat.getDrawable(getResources(), R.drawable.u_logo, null),"asignatura1");
-        CardView_Professor_Data asignatura2 = new CardView_Professor_Data(ResourcesCompat.getDrawable(getResources(), R.drawable.u_logo, null),"asignatura2");
-        CardView_Professor_Data asignatura3 = new CardView_Professor_Data(ResourcesCompat.getDrawable(getResources(), R.drawable.u_logo, null),"asignatura3");
-        CardView_Professor_Data asignatura4 = new CardView_Professor_Data(ResourcesCompat.getDrawable(getResources(), R.drawable.u_logo, null),"asignatura4");
-        CardView_Professor_Data asignatura5 = new CardView_Professor_Data(ResourcesCompat.getDrawable(getResources(), R.drawable.u_logo, null),"asignatura5");
-        CardView_Professor_Data asignatura6 = new CardView_Professor_Data(ResourcesCompat.getDrawable(getResources(), R.drawable.u_logo, null),"asignatura6");
-        CardView_Professor_Data asignatura7 = new CardView_Professor_Data(ResourcesCompat.getDrawable(getResources(), R.drawable.u_logo, null),"asignatura7");
-        CardView_Professor_Data asignatura8 = new CardView_Professor_Data(ResourcesCompat.getDrawable(getResources(), R.drawable.u_logo, null),"asignatura8");
+        CardView_Professor_Data asignatura1 = new CardView_Professor_Data(ResourcesCompat.getDrawable(getResources(), R.drawable.radius_logo, null),"asignatura1");
+        CardView_Professor_Data asignatura2 = new CardView_Professor_Data(ResourcesCompat.getDrawable(getResources(), R.drawable.radius_logo, null),"asignatura2");
+        CardView_Professor_Data asignatura3 = new CardView_Professor_Data(ResourcesCompat.getDrawable(getResources(), R.drawable.radius_logo, null),"asignatura3");
+        CardView_Professor_Data asignatura4 = new CardView_Professor_Data(ResourcesCompat.getDrawable(getResources(), R.drawable.radius_logo, null),"asignatura4");
+        CardView_Professor_Data asignatura5 = new CardView_Professor_Data(ResourcesCompat.getDrawable(getResources(), R.drawable.radius_logo, null),"asignatura5");
+        CardView_Professor_Data asignatura6 = new CardView_Professor_Data(ResourcesCompat.getDrawable(getResources(), R.drawable.radius_logo, null),"asignatura6");
+        CardView_Professor_Data asignatura7 = new CardView_Professor_Data(ResourcesCompat.getDrawable(getResources(), R.drawable.radius_logo, null),"asignatura7");
+        CardView_Professor_Data asignatura8 = new CardView_Professor_Data(ResourcesCompat.getDrawable(getResources(), R.drawable.radius_logo, null),"asignatura8");
         CardView_Professor_Data[] elementos = {asignatura1,asignatura2,asignatura3,asignatura4,asignatura5,asignatura6,asignatura7,asignatura8};
         mAdapterSubjects = new RecyclerAdapter_Professor_Data(elementos);
         mRecyclerViewSubjects.setAdapter(mAdapterSubjects);
