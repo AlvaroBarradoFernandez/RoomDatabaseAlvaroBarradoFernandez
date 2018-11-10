bbb420018ab65bd2a6acc6ccb11becf290ca2a92
DSMan97 <guillecrespoaguayo@gmail.com>
2018-11-10 19:44:01 +0100

    Continuando con los fragments

--- app/src/main/java/com/dsman/uaapp/FormDataFrag1.java
+++ app/src/main/java/com/dsman/uaapp/FormDataFrag1.java
@@ -331,23 +331,23 @@ public class FormDataFrag1 extends FragmentActivity {
 
                     if (findViewById(R.id.Frag1) != null) {
 
-                        // However, if we're being restored from a previous state,
-                        // then we don't need to do anything and should return or else
-                        // we could end up with overlapping fragments.
-                        if (savedInstanceState != null) {
-                            return;
-                        }
-
-                        // Create a new Fragment to be placed in the activity layout
-                        HeadlinesFragment firstFragment = new HeadlinesFragment();
-
-                        // In case this activity was started with special instructions from an
-                        // Intent, pass the Intent's extras to the fragment as arguments
-                        firstFragment.setArguments(getIntent().getExtras());
-
-                        // Add the fragment to the 'fragment_container' FrameLayout
-                        getSupportFragmentManager().beginTransaction()
-                                .add(R.id.Frag1, firstFragment).commit();
+//                        // However, if we're being restored from a previous state,
+//                        // then we don't need to do anything and should return or else
+//                        // we could end up with overlapping fragments.
+//                        if (savedInstanceState != null) {
+//                            return;
+//                        }
+//
+//                        // Create a new Fragment to be placed in the activity layout
+//                        HeadlinesFragment firstFragment = new HeadlinesFragment();
+//
+//                        // In case this activity was started with special instructions from an
+//                        // Intent, pass the Intent's extras to the fragment as arguments
+//                        firstFragment.setArguments(getIntent().getExtras());
+//
+//                        // Add the fragment to the 'fragment_container' FrameLayout
+//                        getSupportFragmentManager().beginTransaction()
+//                                .add(R.id.Frag1, firstFragment).commit();
                     }
 
 
