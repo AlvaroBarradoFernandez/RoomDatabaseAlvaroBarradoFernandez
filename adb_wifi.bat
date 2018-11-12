88d212d4e8eba3b591c73980085ca6c58b4e53c5
DSMan97 <guillecrespoaguayo@gmail.com>
2018-11-12 16:07:06 +0100

    adb Wifi Script añadido (comando adb_wifi.bat) v3

--- adb_wifi.bat
+++ adb_wifi.bat
@@ -2,12 +2,15 @@
 color 0f
 adb kill-server
 adb start-server
+echo Conecte el Smarthphone por USB
+set/p Pulse Intro para confirmar que ha sido conectado
 adb devices
 echo;
 echo Elige el puerto:
 set/p port=
 echo;
 adb tcpip :%port%
+echo Desconete el Cable USB
 echo El puerto elegido es %port%
 echo;
 echo Escribe la IP de tu dispositivo:
