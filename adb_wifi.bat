8eac18a54e97a33f24b2d51dc99d93130d42d41e
DSMan97 <guillecrespoaguayo@gmail.com>
2018-11-12 15:36:27 +0100

    adb Wifi Script añadido (comando adb_wifi.bat)

---
+++ adb_wifi.bat
@@ -0,0 +1,20 @@
+@echo off
+color 0f
+adb devices
+echo;
+echo Elige el puerto:
+set/p port=
+echo;
+adb tcpip :%port%
+echo El puerto elegido es %port%
+echo;
+echo Escribe la IP de tu dispositivo:
+set/p ip=
+echo;
+echo La ip elegida es %ip%
+echo Conectando con %ip%:%port% ...
+echo;
+adb connect %ip%:%port%
+echo Pulse Intro para Salir
+pause>nul
+exit
