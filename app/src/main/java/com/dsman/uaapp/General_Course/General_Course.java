1318e1dc0ae15a38e68444598b7a582eaf0ee8c4
DSMan97 <guillecrespoaguayo@gmail.com>
2018-11-14 10:38:22 +0100

    Logout Arreglado

--- app/src/main/java/com/dsman/uaapp/General_Course/General_Course.java
+++ app/src/main/java/com/dsman/uaapp/General_Course/General_Course.java
@@ -150,6 +150,7 @@ public class General_Course extends AppCompatActivity implements NavigationView.
     }
 
     public void onclicklogout(MenuItem item) {
+        //TODO Intentar arreglar el Warning
         Intent navigate = new Intent(General_Course.this, MainActivity.class);
         navigate.setFlags(navigate.FLAG_ACTIVITY_NEW_TASK | navigate.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(navigate);
